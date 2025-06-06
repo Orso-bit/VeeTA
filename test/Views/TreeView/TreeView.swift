3abea8140915b5dd1fceff7d946bd863a2d86123
AlesR000 <alessandrorippa00@outlook.it>
2025-06-06 15:14:35 +0200

    deleted measurement view perchè già inserita all'interno di tree view

--- test/Views/TreeView/TreeView.swift
+++ test/Views/TreeView/TreeView.swift
@@ -92,7 +92,121 @@ struct TreeView: View {
                 }
                 .padding(.vertical,25)
                 
-                MeasurementView(tree: tree)
+                
+                VStack{
+                    HStack{
+         
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 15)
+                                    .frame(height:100)
+                                    .foregroundStyle(.accent)
+                                
+                                VStack{
+                                    Image(systemName: "ruler.fill")
+                                        .foregroundStyle(.white)
+                                    
+                                        VStack{
+                                            Text(tree.wrappedHeight + "m")
+                                                .lineLimit(1)
+                                                .font(.title)
+                                                .fontWeight(.regular)
+                                                .foregroundColor(Color.white)
+                                        }
+                                    
+                                    
+                                    Text("height")
+                                        .font(.title3)
+                                        .fontWeight(.thin)
+                                        .foregroundColor(Color.white)
+                                }
+                                
+                            }
+                        
+                        
+                        
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 15)
+                                    .frame(height:100)
+                                    .foregroundStyle(.accent)
+                                
+                                VStack{
+                                    Image(systemName: "ruler.fill")
+                                        .foregroundStyle(.white)
+                                    
+                                    VStack{
+                                        Text(tree.wrappedLength + "m")
+                                            .lineLimit(1)
+                                            .font(.title)
+                                            .fontWeight(.regular)
+                                            .foregroundColor(Color.white)
+                                    }
+                                    
+                                    Text("trunk diameter")
+                                        .font(.title3)
+                                        .fontWeight(.thin)
+                                        .foregroundColor(Color.white)
+                                }
+                                
+                                
+                            }
+                        
+                        
+                    }
+                    HStack{
+                        //if let current
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 15)
+                                .frame(height:100)
+                                .foregroundStyle(.accent)
+                            
+                            VStack{
+                                Image(systemName: "angle")
+                                    .foregroundStyle(.white)
+                                
+                                VStack{
+                                    Text(tree.wrappedInclination + "°")
+                                        .lineLimit(1)
+                                        .font(.title)
+                                        .fontWeight(.regular)
+                                        .foregroundColor(Color.white)
+                                }
+                                
+                                Text("inclination")
+                                    .font(.title3)
+                                    .fontWeight(.thin)
+                                    .foregroundColor(Color.white)
+                            }
+                    
+                            
+                        }
+                        
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 15)
+                                .frame(height:100)
+                                .foregroundStyle(.accent)
+                            
+                            VStack{
+                                Image(systemName: "leaf.fill")
+                                    .foregroundStyle(.white)
+                                
+                                VStack{
+                                    Text(tree.wrappedDiameter + "m²")
+                                        .lineLimit(1)
+                                        .font(.title)
+                                        .fontWeight(.regular)
+                                        .foregroundColor(Color.white)
+                                }
+                                
+                                Text("crown projection")
+                                    .font(.title3)
+                                    .fontWeight(.thin)
+                                    .foregroundColor(Color.white)
+                            }
+                    
+                            
+                        }
+                    }
+                }
                     .padding(.horizontal,10)
                     .padding(.vertical,25)
                 
